fc983481e044c700c0700d11b120d4670f0b9f5b
InTr0p <christianolivare01@gmail.com>
2022-02-03 20:24:07 +0000

    actualizacion landing page 5

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -2,7 +2,7 @@ import propTypes from "prop-types";
 import React from "react";
 const Footer = (props) => {
 	return (
-		<footer className="d-flex flex-wrap bg-dark d-flex justify-content-center">
+		<footer className="d-flex flex-wrap bg-dark d-flex justify-content-center mt-5 py-4 ">
 			<p className=" text-white ">{props.description}</p>
 		</footer>
 	);
