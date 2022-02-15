bc5edfbdebda7a87e4c066e13f1c399b9af3708d
InTr0p <christianolivare01@gmail.com>
2022-02-15 18:22:43 +0000

    arreglo 3/FINAL landing page

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -2,54 +2,14 @@ import PropTypes from "prop-types";
 import React from "react";
 const Card = (props) => {
 	return (
-		<div className="row">
-			<div className="col-md-3">
-				<div className="card">
-					<img src={props.image} className="card-img-top" />
-					<div className="card-body">
-						<h5 className="card-title">{props.title}</h5>
-						<p className="card-text">{props.description}</p>
-						<a href="#" className="btn btn-primary">
-							{props.buttonTittle}
-						</a>
-					</div>
-				</div>
-			</div>
-			<div className="col-md-3">
-				<div className="card">
-					<img src={props.image} className="card-img-top" />
-					<div className="card-body">
-						<h5 className="card-title">{props.title}</h5>
-						<p className="card-text">{props.description}</p>
-						<a href="#" className="btn btn-primary">
-							{props.buttonTittle}
-						</a>
-					</div>
-				</div>
-			</div>
-			<div className="col-md-3">
-				<div className="card">
-					<img src={props.image} className="card-img-top" />
-					<div className="card-body">
-						<h5 className="card-title">{props.title}</h5>
-						<p className="card-text">{props.description}</p>
-						<a href="#" className="btn btn-primary">
-							{props.buttonTittle}
-						</a>
-					</div>
-				</div>
-			</div>
-			<div className="col-md-3">
-				<div className="card">
-					<img src={props.image} className="card-img-top" />
-					<div className="card-body">
-						<h5 className="card-title">{props.title}</h5>
-						<p className="card-text">{props.description}</p>
-						<a href="#" className="btn btn-primary">
-							{props.buttonTittle}
-						</a>
-					</div>
-				</div>
+		<div className="card">
+			<img src={props.image} className="card-img-top" />
+			<div className="card-body">
+				<h5 className="card-title">{props.title}</h5>
+				<p className="card-text">{props.description}</p>
+				<a href="#" className="btn btn-primary">
+					{props.buttonTittle}
+				</a>
 			</div>
 		</div>
 	);
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -15,12 +15,43 @@ const Home = () => {
 					description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 					descriptionButton="Call to action!"
 				/>
-				<Card
-					title="Card title "
-					description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-					buttonTittle="Find Out More"
-					image={"https://picsum.photos/200"}
-				/>
+				<div className="row">
+					<div className="col-md-3">
+						<Card
+							title="Card title "
+							description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+							buttonTittle="Find Out More"
+							image={"https://picsum.photos/200"}
+						/>
+					</div>
+
+					<div className="col-md-3">
+						<Card
+							title="Card title "
+							description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+							buttonTittle="Find Out More"
+							image={"https://picsum.photos/200"}
+						/>
+					</div>
+
+					<div className="col-md-3">
+						<Card
+							title="Card title "
+							description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+							buttonTittle="Find Out More"
+							image={"https://picsum.photos/200"}
+						/>
+					</div>
+
+					<div className="col-md-3">
+						<Card
+							title="Card title "
+							description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+							buttonTittle="Find Out More"
+							image={"https://picsum.photos/200"}
+						/>
+					</div>
+				</div>
 			</div>
 			<Footer description="Copyright © Your Website 2019" />
 		</>
